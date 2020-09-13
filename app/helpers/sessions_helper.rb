14cbf66699589582a96cdf46699f2c5895cb4b3d
yuukiyokoyama <yuukitigars89@gmail.com>
2020-09-13 20:44:18 +0900

    10章　削除ボタン以外完成

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -25,22 +25,41 @@ module SessionsHelper
     end
   end
 
-  # ユーザーがログインしていればtrue、その他ならfalseを返す
-  def logged_in?
-    !current_user.nil?
-  end
+    # 渡されたユーザーがカレントユーザーであればtrueを返す
+    def current_user?(user)
+      user && user == current_user
+    end
 
-  # 永続的セッションを破棄する
-  def forget(user)
-    user.forget
-    cookies.delete(:user_id)
-    cookies.delete(:remember_token)
-  end
+    # ユーザーがログインしていればtrue、その他ならfalseを返す
+    def logged_in?
+      !current_user.nil?
+    end
 
-# 現在のユーザーをログアウトする
-def log_out
-  session.delete(:user_id)
-  @current_user = nil
-end
-end
+    # 永続的セッションを破棄する
+    def forget(user)
+      # user.forget
+      cookies.delete(:user_id)
+      cookies.delete(:remember_token)
+    end
+
+    # 現在のユーザーをログアウトする
+    def log_out
+      forget(current_user)
+      session.delete(:user_id)
+      @current_user = nil
+    end
+    end
+
+    # 記憶したURL（もしくはデフォルト値）にリダイレクト
+    def redirect_back_or(default)
+      redirect_to(session[:forwarding_url] || default)
+      session.delete(:forwarding_url)
+    end
+  
+    # アクセスしようとしたURLを覚えておく
+    def store_location
+      session[:forwarding_url] = request.original_url if request.get?
+    end
+
+
 
